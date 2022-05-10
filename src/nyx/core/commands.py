d251abdfa51f84fde1f9814d483a06ef4bdb5324
sowwic <47799401+sowwic@users.noreply.github.com>
2022-05-10 17:14:00 +0100

    Add attribute commands

--- src/nyx/core/commands.py
+++ src/nyx/core/commands.py
@@ -7,8 +7,12 @@ from nyx.core import Stage
 
 
 class NyxCommand(QtWidgets.QUndoCommand):
-    def __init__(self, text: str, parent: QtWidgets.QUndoCommand = None) -> None:
-        super().__init__(text, parent)
+    def __init__(self,
+                 stage: "Stage",
+                 text: str,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(text, parent_command=parent_command)
+        self.stage = stage
 
 
 class CreateNodeCommand(NyxCommand):
@@ -16,9 +20,9 @@ class CreateNodeCommand(NyxCommand):
                  stage: Stage,
                  node_name: str = "node",
                  parent_path: str = None,
-                 position=None, parent_command=None) -> None:
-        super().__init__("Create Node", parent=parent_command)
-        self.stage = stage
+                 position: "list[float, float]" = None,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage=stage, text="Create Node", parent_command=parent_command)
         self.position = position
         self.parent_path = parent_path
         self.node_name = node_name
@@ -39,11 +43,11 @@ class CreateNodeCommand(NyxCommand):
 
 
 class DeleteNodeCommand(NyxCommand):
-    def __init__(self, stage: Stage,
+    def __init__(self,
+                 stage: Stage,
                  node: "Node | pathlib.PurePosixPath | str",
-                 parent: QtWidgets.QUndoCommand = None) -> None:
-        super().__init__("Delete node", parent)
-        self.stage = stage
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage=stage, text="Delete node", parent_command=parent_command)
         node = self.stage.node(node)
         self.node_data = node.serialize()
         self.setText(f"Delete node ({node.path.as_posix()})")
@@ -62,12 +66,12 @@ class DeleteNodeCommand(NyxCommand):
 
 
 class RenameNodeCommand(NyxCommand):
-    def __init__(self, stage: Stage,
+    def __init__(self,
+                 stage: Stage,
                  node: "Node | pathlib.PurePosixPath | str",
                  new_name: str,
                  parent_command: QtWidgets.QUndoCommand = None) -> None:
-        super().__init__("Rename node", parent=parent_command)
-        self.stage = stage
+        super().__init__(stage=stage, text="Rename node", parent_command=parent_command)
 
         node = self.stage.node(node)
         self.new_name = new_name
@@ -75,7 +79,7 @@ class RenameNodeCommand(NyxCommand):
         self.old_name = node.name
         self.node_data = node.serialize()
 
-        self.setText(f"Rename node ({self.old_name} -> {self.new_name})")
+        self.setText(f"Rename node ({node.path.as_posix()} -> {self.new_name})")
 
     def redo(self) -> None:
         node = self.stage.node(self.node_data.get("path"))
@@ -86,4 +90,106 @@ class RenameNodeCommand(NyxCommand):
     def undo(self) -> None:
         node = self.stage.node(self.renamed_node_data.get("path"))
         node.rename(self.old_name)
+        return super().undo()
+
+
+class AddNodeAttributeCommand(NyxCommand):
+    def __init__(self,
+                 stage: "Stage",
+                 node: "Node | pathlib.PurePosixPath | str",
+                 attr_name: str,
+                 attr_value=None,
+                 attr_resolve=True,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage, "Add attr", parent_command)
+        self.node_path = self.stage.node(node).path
+        self.attr_name = attr_name
+        self.attr_value = attr_value
+        self.attr_resolve = attr_resolve
+        self.setText(f"{self.text()} '{self.attr_name}' to {self.node_path.as_posix()}")
+
+    def redo(self) -> None:
+        node = self.stage.node(self.node_path)
+        node.add_attr(self.attr_name, value=self.attr_value, resolve=self.attr_resolve)
+        return super().redo()
+
+    def undo(self) -> None:
+        node = self.stage.node(self.node_path)
+        node.delete_attr(self.attr_name)
+        return super().undo()
+
+
+class DeleteNodeAttributeCommand(NyxCommand):
+    def __init__(self,
+                 stage: "Stage",
+                 node: "Node | pathlib.PurePosixPath | str",
+                 attr_name: str,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage, "Delete attr", parent_command)
+        self.node_path = self.stage.node(node).path
+        self.attr_name = attr_name
+        self.attr_serialized = None
+        self.setText(f"{self.text()} '{self.attr_name}' from {self.node_path.as_posix()}")
+
+    def redo(self) -> None:
+        node = self.stage.node(self.node_path)
+        self.attr_serialized = node.attr(self.attr_name).serialize()
+        node.delete_attr(self.attr_name)
+        return super().redo()
+
+    def undo(self) -> None:
+        node = self.stage.node(self.node_path)
+        attr_name = self.attr_serialized["name"]
+        node.add_attr(attr_name)
+        node.attr(attr_name).deserialize(self.attr_serialized, {}, restore_id=True)
+        return super().undo()
+
+
+class RenameNodeAttributeCommand(NyxCommand):
+    def __init__(self,
+                 stage: "Stage",
+                 node: "Node | pathlib.PurePosixPath | str",
+                 attr_name: str,
+                 new_attr_name: str,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage, "Rename attr", parent_command)
+        self.node_path = self.stage.node(node).path
+        self.attr_name = attr_name
+        self.new_attr_name = new_attr_name
+        self.setText(f"{self.text()} '{self.attr_name}' \
+            -> '{self.new_attr_name}' on {self.node_path.as_posix()}")
+
+    def redo(self) -> None:
+        node = self.stage.node(self.node_path)
+        node.rename_attr(self.attr_name, self.new_attr_name)
+        return super().redo()
+
+    def undo(self) -> None:
+        node = self.stage.node(self.node_path)
+        node.rename_attr(self.new_attr_name, self.attr_name)
+        return super().undo()
+
+
+class SetNodeAttributeCommand(NyxCommand):
+    def __init__(self,
+                 stage: "Stage",
+                 node: "Node | pathlib.PurePosixPath | str",
+                 attr_name: str,
+                 attr_value=None,
+                 parent_command: QtWidgets.QUndoCommand = None) -> None:
+        super().__init__(stage, "Set attr", parent_command)
+        self.node_path = self.stage.node(node).path
+        self.attr_name = attr_name
+        self.attr_value = attr_value
+        self.old_attr_value = None
+
+    def redo(self) -> None:
+        node = self.stage.node(self.node_path)
+        self.old_attr_value = node.attr(self.attr_name).get(raw=True)
+        node.set_attr(self.attr_name, self.attr_value)
+        return super().redo()
+
+    def undo(self) -> None:
+        node = self.stage.node(self.node_path)
+        node.set_attr(self.attr_name, self.old_attr_value)
         return super().undo()
